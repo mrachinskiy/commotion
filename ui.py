ac3e9e408c02ad7d4a87ea57e2390f51677dd3df
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2021-07-02 22:25:32 +0400

    Update copyright year

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  Commotion motion graphics add-on for Blender.
-#  Copyright (C) 2014-2020  Mikhail Rachinskiy
+#  Copyright (C) 2014-2021  Mikhail Rachinskiy
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by

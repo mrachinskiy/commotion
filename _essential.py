9ae80ea37d03c023e2166a59976f2d285f7f3177
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2022-02-12 20:53:14 +0400

    Cleanup: use SPDX license headers

--- _essential.py
+++ _essential.py
@@ -1,26 +1,7 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  Essential utility functions.
-#  Copyright (C) 2021-2022  Mikhail Rachinskiy
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-# ##### END GPL LICENSE BLOCK #####
-
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright 2021-2022 Mikhail Rachinskiy
 
 # v1.1.1
-
 
 from __future__ import annotations
 from pathlib import Path
